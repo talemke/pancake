0ef48725f3b596f5db98fd04829491e8fd9774a4
Tassilo <tassia710@gmail.com>
2020-09-16 21:16:37 +0200

    Removed old index

--- src/main/java/net/tassia/pancake/http/routes/inbox/InboxRoutes.java
+++ src/main/java/net/tassia/pancake/http/routes/inbox/InboxRoutes.java
@@ -13,18 +13,11 @@ import java.util.Collection;
 import java.util.Date;
 
 public class InboxRoutes {
-	private final HttpView view;
-	private final HttpView mailNavView;
-	private final HttpView mailView;
-	private final SimpleDateFormat format1, format2;
+	private final SimpleDateFormat format;
 
 	/* Constructor */
 	public InboxRoutes() {
-		this.view = new HttpView("/views/inbox/index.html");
-		this.mailNavView = new HttpView("/views/inbox/mail_nav_entry.html");
-		this.mailView = new HttpView("/views/inbox/email.html");
-		this.format1 = new SimpleDateFormat("dd.MM.yyyy");
-		this.format2 = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+		this.format = new SimpleDateFormat("dd.MM.yyyy");
 	}
 	/* Constructor */
 
@@ -48,44 +41,9 @@ public class InboxRoutes {
 	protected void addMailNav(GenericPancakeView view, Collection<Email> mails, Email focus) {
 		for (Email email : mails) {
 			boolean active = focus != null && focus.getUUID().equals(email.getUUID());
-			String description = email.getSender() + " - " + format1.format(new Date(email.getTimestamp()));
+			String description = email.getSender() + " - " + format.format(new Date(email.getTimestamp()));
 			view.addMailNav("/mail/" + email.getUUID().toString(), "N/A", description, active); // TODO: Subject
 		}
-	}
-
-	protected byte[] generateView(Pancake pancake, HttpRequest request, Collection<Email> list, Email focus) {
-		String mailNav = "";
-		for (Email email : list) {
-			boolean isFocussed = focus != null && focus.getUUID().equals(email.getUUID());
-			mailNav += mailNavView.view(
-				new String[] { "mail_id", email.getUUID().toString() },
-				new String[] { "mail_subject", "N/A" },
-				new String[] { "mail_author", email.getSender() },
-				new String[] { "mail_date", format1.format(new Date(email.getTimestamp())) },
-				new String[] { "mail_active", isFocussed ? "mailnav-button-active" : "" }
-			);
-		}
-
-		String mail;
-		if (focus != null) {
-			mail = mailView.view(
-				new String[] { "mail_id", focus.getUUID().toString() },
-				new String[] { "mail_subject", "N/A" },
-				new String[] { "mail_date", format2.format(new Date(focus.getTimestamp())) },
-				new String[] { "mail_size", focus.getData().length + " bytes" },
-				new String[] { "mail_sender", focus.getSender() },
-				new String[] { "mail_recipient", focus.getRecipient() },
-				new String[] { "mail_content", new String(focus.getData(), StandardCharsets.UTF_8) }
-			);
-		} else {
-			mail = "<span class=\"pos-absolute pos-center\">No mail selected.</span>";
-		}
-
-		return view.viewData(
-			new String[] { "username", request.getAuth().getName() },
-			new String[] { "mail_nav", mailNav },
-			new String[] { "email", mail }
-		);
 	}
 	/* Generate View */
 
