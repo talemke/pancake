3077fbb2778ed194b571057e06d21c451b383e4f
Tassilo <tassia710@gmail.com>
2020-09-16 15:05:09 +0200

    Enhanced config

--- src/main/java/net/tassia/pancake/PancakeConfiguration.java
+++ src/main/java/net/tassia/pancake/PancakeConfiguration.java
@@ -16,6 +16,17 @@ public class PancakeConfiguration {
 	public int httpPort = 8080;
 	public int httpBacklog = 0;
 
+	public int smtpPort = 25;
+	public int smtpBacklog = 50;
+	public boolean smtpEnableTLS = false;
+	public boolean smtpHideTLS = false;
+	public boolean smtpRequireTLS = false;
+	public boolean smtpDisableReceivedHeaders = false;
+	public int smtpMaxConnections = 1000;
+	public int smtpConnectionTimeout = 60000;
+	public int smtpMaxRecipients = 1000;
+	public int smtpMaxMessageSize = 0;
+
 
 
 	public enum StorageDriver {
@@ -34,13 +45,26 @@ public class PancakeConfiguration {
 
 		try {
 			config.storageDriver = StorageDriver.valueOf(p.getProperty("STORAGE_DRIVER"));
+
 			config.mysqlHostname = p.getProperty("MYSQL_HOSTNAME");
 			config.mysqlPort = Integer.parseInt(p.getProperty("MYSQL_PORT"));
 			config.mysqlDatabase = p.getProperty("MYSQL_DATABASE");
 			config.mysqlUsername = p.getProperty("MYSQL_USERNAME");
 			config.mysqlPassword = p.getProperty("MYSQL_PASSWORD");
+
 			config.httpPort = Integer.parseInt(p.getProperty("HTTP_PORT"));
 			config.httpBacklog = Integer.parseInt(p.getProperty("HTTP_BACKLOG"));
+
+			config.smtpPort = Integer.parseInt(p.getProperty("SMTP_PORT"));
+			config.smtpBacklog = Integer.parseInt(p.getProperty("SMTP_BACKLOG"));
+			config.smtpEnableTLS = Boolean.parseBoolean(p.getProperty("SMTP_ENABLE_TLS"));
+			config.smtpHideTLS = Boolean.parseBoolean(p.getProperty("SMTP_HIDE_TLS"));
+			config.smtpRequireTLS = Boolean.parseBoolean(p.getProperty("SMTP_REQUIRE_TLS"));
+			config.smtpDisableReceivedHeaders = Boolean.parseBoolean(p.getProperty("SMTP_DISABLE_RECEIVED_HEADERS"));
+			config.smtpMaxConnections = Integer.parseInt(p.getProperty("SMTP_MAX_CONNECTIONS"));
+			config.smtpConnectionTimeout = Integer.parseInt(p.getProperty("SMTP_CONNECTION_TIMEOUT"));
+			config.smtpMaxRecipients = Integer.parseInt(p.getProperty("SMTP_MAX_RECIPIENTS"));
+			config.smtpMaxMessageSize = Integer.parseInt(p.getProperty("SMTP_MAX_MESSAGE_SIZE"));
 		} catch (Throwable ex) {
 			throw new IOException("Invalid configuration, try deleting the .env file.", ex);
 		}
@@ -56,13 +80,26 @@ public class PancakeConfiguration {
 		Properties p = new Properties();
 
 		p.setProperty("STORAGE_DRIVER", config.storageDriver.name());
+
 		p.setProperty("MYSQL_HOSTNAME", config.mysqlHostname);
 		p.setProperty("MYSQL_PORT", Integer.toString(config.mysqlPort));
 		p.setProperty("MYSQL_DATABASE", config.mysqlDatabase);
 		p.setProperty("MYSQL_USERNAME", config.mysqlUsername);
 		p.setProperty("MYSQL_PASSWORD", config.mysqlPassword);
+
 		p.setProperty("HTTP_PORT", Integer.toString(config.httpPort));
 		p.setProperty("HTTP_BACKLOG", Integer.toString(config.httpBacklog));
+
+		p.setProperty("SMTP_PORT", Integer.toString(config.smtpPort));
+		p.setProperty("SMTP_BACKLOG", Integer.toString(config.smtpBacklog));
+		p.setProperty("SMTP_ENABLE_TLS", Boolean.toString(config.smtpEnableTLS));
+		p.setProperty("SMTP_HIDE_TLS", Boolean.toString(config.smtpHideTLS));
+		p.setProperty("SMTP_REQUIRE_TLS", Boolean.toString(config.smtpRequireTLS));
+		p.setProperty("SMTP_DISABLE_RECEIVED_HEADERS", Boolean.toString(config.smtpDisableReceivedHeaders));
+		p.setProperty("SMTP_MAX_CONNECTIONS", Integer.toString(config.smtpMaxConnections));
+		p.setProperty("SMTP_CONNECTION_TIMEOUT", Integer.toString(config.smtpConnectionTimeout));
+		p.setProperty("SMTP_MAX_RECIPIENTS", Integer.toString(config.smtpMaxRecipients));
+		p.setProperty("SMTP_MAX_MESSAGE_SIZE", Integer.toString(config.smtpMaxMessageSize));
 
 		FileOutputStream fos = new FileOutputStream(file);
 		String version = String.format("%d.%d.%d", Pancake.VERSION_MAJOR, Pancake.VERSION_MINOR, Pancake.VERSION_BUILD);
--- src/main/java/net/tassia/pancake/smtp/PancakeSMTP.java
+++ src/main/java/net/tassia/pancake/smtp/PancakeSMTP.java
@@ -1,6 +1,7 @@
 package net.tassia.pancake.smtp;
 
 import net.tassia.pancake.Pancake;
+import org.subethamail.smtp.Version;
 import org.subethamail.smtp.server.SMTPServer;
 
 public class PancakeSMTP {
@@ -9,8 +10,17 @@ public class PancakeSMTP {
 	public PancakeSMTP(Pancake pancake) {
 		PancakeMessageHandlerFactory factory = new PancakeMessageHandlerFactory(pancake);
 		server = new SMTPServer(factory);
-		server.setSoftwareName("PancakeSMTP");
-		server.setPort(2500);
+		server.setSoftwareName("PancakeSMTP - " + Version.getSpecification());
+		server.setPort(pancake.getConfig().smtpPort);
+		server.setBacklog(pancake.getConfig().smtpBacklog);
+		server.setEnableTLS(pancake.getConfig().smtpEnableTLS);
+		server.setHideTLS(pancake.getConfig().smtpHideTLS);
+		server.setRequireTLS(pancake.getConfig().smtpRequireTLS);
+		server.setDisableReceivedHeaders(pancake.getConfig().smtpDisableReceivedHeaders);
+		server.setMaxConnections(pancake.getConfig().smtpMaxConnections);
+		server.setConnectionTimeout(pancake.getConfig().smtpConnectionTimeout);
+		server.setMaxRecipients(pancake.getConfig().smtpMaxRecipients);
+		server.setMaxMessageSize(pancake.getConfig().smtpMaxMessageSize);
 	}
 
 	public void start() {
