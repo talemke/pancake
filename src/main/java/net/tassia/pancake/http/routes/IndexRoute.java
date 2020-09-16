c473c06c8271a560f4e234792619cc8ec0a00e84
Tassilo <tassia710@gmail.com>
2020-09-16 21:15:42 +0200

    Cleaned up index route

--- src/main/java/net/tassia/pancake/http/routes/IndexRoute.java
+++ src/main/java/net/tassia/pancake/http/routes/IndexRoute.java
@@ -2,28 +2,17 @@ package net.tassia.pancake.http.routes;
 
 import net.tassia.pancake.Pancake;
 import net.tassia.pancake.http.HttpRequest;
-import net.tassia.pancake.http.HttpView;
-import net.tassia.pancake.http.HttpViewRoute;
+import net.tassia.pancake.http.HttpRoute;
 
-public class IndexRoute extends HttpViewRoute {
-	private final HttpView mailNavView;
-	private final HttpView emailView;
-
-	public IndexRoute() {
-		super("/views/inbox/index.html");
-		this.mailNavView = new HttpView("/views/inbox/mail_nav_entry.html");
-		this.emailView = new HttpView("/views/inbox/email.html");
-	}
+public class IndexRoute implements HttpRoute {
 
 	@Override
 	public byte[] route(Pancake pancake, HttpRequest request, String[] matches) {
-		// Check auth
 		if (!request.checkAuth()) {
 			request.redirect("/auth/login");
-			return null;
+		} else {
+			request.redirect("/inbox");
 		}
-
-		request.redirect("/inbox");
 		return null;
 	}
 
