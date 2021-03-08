bb26e28aae02613416734af9e7de19c3dc42f549
Tassilo <tassia710@gmail.com>
2021-03-08 20:27:12 +0100

    Create Entrypoint.kt

---
+++ src/main/java/net/tassia/pancake/Entrypoint.kt
@@ -0,0 +1,11 @@
+package net.tassia.pancake
+
+/**
+ * The JVM entrypoint for Pancake.
+ *
+ * @since Pancake 1.0
+ * @author Tassilo
+ */
+fun main(args: Array<String>) {
+	PancakeLauncher.new(args)
+}
