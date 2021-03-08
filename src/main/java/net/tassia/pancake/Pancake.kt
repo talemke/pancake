b2cb380e9c058d3dc3f43484277fdbbb7c9e9ecc
Tassilo <tassia710@gmail.com>
2021-03-08 15:44:43 +0100

    Added base

---
+++ src/main/java/net/tassia/pancake/Pancake.kt
@@ -0,0 +1,10 @@
+package net.tassia.pancake
+
+/**
+ * The base class for Pancake.
+ *
+ * @since Pancake 1.0
+ * @author Tassilo
+ */
+class Pancake {
+}
---
+++ src/main/java/net/tassia/pancake/PancakeLauncher.kt
@@ -0,0 +1,22 @@
+package net.tassia.pancake
+
+/**
+ * This object is used to launch a new [Pancake] instance.
+ *
+ * @since Pancake 1.0
+ * @author Tassilo
+ */
+object PancakeLauncher {
+
+	/**
+	 * Launches a new [Pancake] instance.
+	 *
+	 * @param args command-line arguments
+	 * @return the instance
+	 */
+	fun new(args: Array<String>): Pancake {
+		// TODO
+		return Pancake()
+	}
+
+}
