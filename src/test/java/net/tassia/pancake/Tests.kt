ae8486d70c5b3f6a9b2317c36142500ff3033c24
Tassilo <tassia710@gmail.com>
2021-03-09 17:00:27 +0100

    Create Tests.kt

---
+++ src/test/java/net/tassia/pancake/Tests.kt
@@ -0,0 +1,7 @@
+package net.tassia.pancake
+
+const val LETTERS_UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const val LETTERS_LOWER = "abcdefghijklmnopqrstuvwxyz"
+const val DIGITS = "0123456789"
+
+const val ITERATIONS = 4096 * 32
