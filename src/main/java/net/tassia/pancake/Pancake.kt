9e774249fc133c4c08bc63ebe780897a28ce8149
Tassilo <tassia710@gmail.com>
2021-03-10 14:02:47 +0100

    Update Pancake.kt

--- src/main/java/net/tassia/pancake/Pancake.kt
+++ src/main/java/net/tassia/pancake/Pancake.kt
@@ -7,4 +7,29 @@ package net.tassia.pancake
  * @author Tassilo
  */
 class Pancake {
+
+	companion object {
+
+		/**
+		 * The hard-coded limit of how long a single mail address ('name@hostname.tld') can be (in bytes).
+		 */
+		const val MAIL_ADDRESS_MAX = 255
+
+		/**
+		 * The hard-coded limit of how long a mail subject can be (in bytes).
+		 */
+		const val MAIL_SUBJECT_MAX = 255
+
+		/**
+		 * The hard-coded limit of how long a mails displayable content can be (in bytes).
+		 */
+		const val MAIL_CONTENT_MAX = Int.MAX_VALUE
+
+		/**
+		 * The hard-coded limit of how many recipients a single mail can have at maximum.
+		 */
+		const val MAIL_RECIPIENTS_MAX = 63
+
+	}
+
 }
