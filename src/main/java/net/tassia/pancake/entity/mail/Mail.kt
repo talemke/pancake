9173838be61b1ed9e736222cacaea4bf6643040e
Tassilo <tassia710@gmail.com>
2021-03-10 19:56:26 +0100

    Update Mail.kt

--- src/main/java/net/tassia/pancake/entity/mail/Mail.kt
+++ src/main/java/net/tassia/pancake/entity/mail/Mail.kt
@@ -17,4 +17,14 @@ class Mail(id: EntityID<UUID>) : Entity<UUID>(id) {
 
 	companion object : EntityClass<UUID, Mail>(MailTable)
 
+	var contentLength by MailTable.contentLength
+	var content by MailTable.content
+
+	var timestamp by MailTable.timestamp
+	var subject by MailTable.subject
+	var sender by MailTable.sender
+	var recipients by MailTable.recipients
+
+	var folder by MailTable.folder
+
 }
