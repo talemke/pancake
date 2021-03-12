c1ef3bbf2d7e0a187ea38fb00f1e53ca0f2bf1ac
Tassilo <tassia710@gmail.com>
2021-03-12 16:00:54 +0100

    Update PancakeLauncher.kt

--- src/main/java/net/tassia/pancake/PancakeLauncher.kt
+++ src/main/java/net/tassia/pancake/PancakeLauncher.kt
@@ -1,5 +1,11 @@
 package net.tassia.pancake
 
+import net.tassia.pancake.config.ConfigIO
+import net.tassia.pancake.config.DatabaseDriver
+import net.tassia.pancake.config.PancakeConfig
+import org.jetbrains.exposed.sql.Database
+import java.io.File
+
 /**
  * This object is used to launch a new [Pancake] instance.
  *
@@ -17,7 +23,21 @@ object PancakeLauncher {
 	 * @return the instance
 	 */
 	fun new(args: Array<String>): Pancake {
-		// TODO
+		// Load configuration
+		val cfg = ConfigIO.load(File("config.yml"), PancakeConfig())
+
+		// Connect to database
+		when (cfg.databaseDriver) {
+			DatabaseDriver.SQLITE -> {
+				Database.connect("jdbc:sqlite:./data.db", "org.sqlite.JDBC")
+			}
+			DatabaseDriver.MYSQL -> {
+				Database.connect("jdbc:mysql://${cfg.mysqlHostname}:${cfg.mysqlPort}/${cfg.mysqlDatabase}",
+					driver = "com.mysql.jdbc.Driver", user = cfg.mysqlUsername, password = cfg.mysqlPassword)
+			}
+		}
+
+		// Launch
 		return Pancake()
 	}
 
