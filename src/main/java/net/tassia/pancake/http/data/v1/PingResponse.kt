2943c7777d35b1b2db4cf96d869b6d78ea3e9bf7
Tassilo <tassia710@gmail.com>
2021-03-14 00:44:39 +0100

    Create PingResponse.kt

---
+++ src/main/java/net/tassia/pancake/http/data/v1/PingResponse.kt
@@ -0,0 +1,17 @@
+package net.tassia.pancake.http.data.v1
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import net.tassia.pancake.Pancake
+
+/**
+ * A basic 'Ping' response.
+ *
+ * @since Pancake 1.0
+ * @author Tassilo
+ */
+class PingResponse {
+
+	@JsonProperty("version")
+	val version: String = Pancake.VERSION.toDisplayString()
+
+}
