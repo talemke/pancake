69f963f9f1dbafacf3eac92c2a26896718fb2757
Tassilo <tassia710@gmail.com>
2021-03-14 16:15:31 +0100

    Update CLI.kt

--- src/main/java/net/tassia/pancake/cli/CLI.kt
+++ src/main/java/net/tassia/pancake/cli/CLI.kt
@@ -1,17 +1,28 @@
 package net.tassia.pancake.cli
 
+import java.util.*
+
 object CLI {
 
-	fun print(message: String = "") {
-		println(message + ANSI.RESET)
+	private val output = System.out
+	private val scanner = Scanner(System.`in`)
+
+
+
+	fun print(message: String = "", newLine: Boolean = true) {
+		if (newLine) {
+			output.println(message + ANSI.RESET)
+		} else {
+			output.print(message + ANSI.RESET)
+		}
 	}
 
-	fun success(message: String = "") {
-		print(ANSI.SUCCESS + Icon.CHECK + " " + message)
-	}
+	fun success(message: String = "") = print(ANSI.SUCCESS + Icon.CHECK + " " + message)
 
-	fun failure(message: String = "") {
-		print(ANSI.FAILURE + Icon.CROSS + " " + message)
-	}
+	fun failure(message: String = "") = print(ANSI.FAILURE + Icon.CROSS + " " + message)
+
+
+
+	fun readLine(): String = scanner.nextLine()
 
 }
