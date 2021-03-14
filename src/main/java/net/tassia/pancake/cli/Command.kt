3a5ddcda0a75dad5249c07ba2311b2a11567babc
Tassilo <tassia710@gmail.com>
2021-03-14 16:39:24 +0100

    Update Command.kt

--- src/main/java/net/tassia/pancake/cli/Command.kt
+++ src/main/java/net/tassia/pancake/cli/Command.kt
@@ -2,8 +2,22 @@ package net.tassia.pancake.cli
 
 import net.tassia.pancake.Pancake
 
+/**
+ * This is the actual command executor.
+ *
+ * @since Pancake 1.0
+ * @author Tassilo
+ */
 fun interface Command {
 
+	/**
+	 * Invoked when the user executes a command.
+	 *
+	 * @param pancake the [Pancake] instance
+	 * @param args the arguments
+	 * @param toggles toggle flags
+	 * @param flags value flags
+	 */
 	fun onCommand(pancake: Pancake, args: List<String>, toggles: Map<String, Boolean>, flags: Map<String, String>)
 
 }
