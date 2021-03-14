15aef1d32322af7360030cf453b32c31d81e0e69
Tassilo <tassia710@gmail.com>
2021-03-14 16:41:58 +0100

    Update HelpCommand.kt

--- src/main/java/net/tassia/pancake/cli/command/HelpCommand.kt
+++ src/main/java/net/tassia/pancake/cli/command/HelpCommand.kt
@@ -3,8 +3,17 @@ package net.tassia.pancake.cli.command
 import net.tassia.pancake.cli.Command
 import net.tassia.pancake.cli.CommandInfo
 
+/**
+ * The 'HELP' command. Shows helpful information.
+ *
+ * @since Pancake 1.0
+ * @author Tassilo
+ */
 object HelpCommand {
 
+	/**
+	 * The command information.
+	 */
 	val Info = CommandInfo(
 		name = "Help",
 		description = "Shows helpful information.",
@@ -14,6 +23,9 @@ object HelpCommand {
 		)
 	)
 
+	/**
+	 * The command executor.
+	 */
 	val Executor = Command { pancake, args, toggles, flags ->
 		TODO()
 	}
