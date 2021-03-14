55bc02dbf0433646595498a5826c5a0e88f40d2a
Tassilo <tassia710@gmail.com>
2021-03-14 12:31:58 +0100

    Create AuthInformation.kt

---
+++ src/main/java/net/tassia/pancake/http/AuthInformation.kt
@@ -0,0 +1,30 @@
+package net.tassia.pancake.http
+
+import net.tassia.pancake.entity.account.Account
+import net.tassia.pancake.entity.group.Group
+import net.tassia.pancake.entity.session.Session
+
+/**
+ * This data class stores authentication information about an HTTP request.
+ *
+ * @since Pancake 1.0
+ * @author Tassilo
+ */
+data class AuthInformation(
+
+	/**
+	 * The session used to authenticate.
+	 */
+	val session: Session,
+
+	/**
+	 * The account who authenticated.
+	 */
+	val account: Account,
+
+	/**
+	 * The group of the account.
+	 */
+	val group: Group
+
+)
