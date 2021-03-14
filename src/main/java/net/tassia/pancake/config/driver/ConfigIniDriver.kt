9fb0bf9156e663ec8401c7b52117e5a32a9657b4
Tassilo <tassia710@gmail.com>
2021-03-14 02:18:07 +0100

    Update ConfigIniDriver.kt

--- src/main/java/net/tassia/pancake/config/driver/ConfigIniDriver.kt
+++ src/main/java/net/tassia/pancake/config/driver/ConfigIniDriver.kt
@@ -5,6 +5,9 @@ import java.io.Reader
 import java.io.Writer
 import java.util.*
 
+private typealias Config = Map<String, String>
+private typealias SectionCommenter = (String?, String?) -> String?
+
 /**
  * The `.ini` file format.
  *
@@ -28,17 +31,59 @@ object ConfigIniDriver : ConfigDriver("ini") {
 		return map
 	}
 
-	override fun write(writer: Writer, map: Map<String, String>) {
-		// Create empty properties
-		val props = Properties()
 
-		// Transform to properties
-		for (entry in map.entries) {
-			props[entry.key] = entry.value
+
+	override fun write(writer: Writer, map: Config) = writeIni(writer, map)
+
+	private fun insertData(section: String?, key: String, value: String, store: MutableMap<String, MutableMap<String, String>>) {
+		val store2 = store[section ?: ""] ?: mutableMapOf()
+		store2[key] = value
+		store[section ?: ""] = store2
+	}
+
+	private fun writeIni(writer: Writer, config: Config, sectionCommenter: SectionCommenter = {_, _ -> null}) {
+		// Load sections
+		val sections = mutableMapOf<String, MutableMap<String, String>>()
+		for (entry in config.entries) {
+			val i = entry.key.lastIndexOf('.')
+			if (i == -1) {
+				insertData(null, entry.key, entry.value, sections)
+			} else {
+				insertData(entry.key.substring(0, i), entry.key.substring(i + 1), entry.value, sections)
+			}
 		}
 
-		// Write properties
-		props.store(writer, null)
+		// Write root section
+		sections[""]?.let { writeIniSection(writer, it, null, sectionCommenter) }
+
+		// Write other section
+		for (entry in sections.entries) {
+			if (entry.key == "") continue
+			writer.writeLine("")
+			writeIniSection(writer, entry.value, entry.key, sectionCommenter)
+		}
+	}
+
+	private fun writeIniSection(writer: Writer, config: Config, sectionName: String?, sectionCommenter: SectionCommenter) {
+		// Write section comment
+		sectionCommenter(sectionName, null)?.let { writer.writeLine(it) }
+
+		// Write section header
+		sectionName?.let { writer.writeLine("[$it]") }
+
+		// Write values
+		for (entry in config.entries) {
+			// Write value comment
+			sectionCommenter(sectionName, entry.key)?.let { writer.writeLine(it) }
+
+			// Write value
+			writer.writeLine(entry.key + " = " + entry.value)
+		}
+	}
+
+	private fun Writer.writeLine(line: String) {
+		write(line)
+		write(System.lineSeparator())
 	}
 
 }
