31ee90e32bbae93501d511198853a7509a621982
Tassilo <tassia710@gmail.com>
2021-03-20 15:34:24 +0100

    Update PancakeHttp.kt

--- src/main/java/net/tassia/pancake/plugins/http/PancakeHttp.kt
+++ src/main/java/net/tassia/pancake/plugins/http/PancakeHttp.kt
@@ -1,14 +1,17 @@
-package net.tassia.pancake.http
+package net.tassia.pancake.plugins.http
 
 import io.ktor.application.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import net.tassia.pancake.Pancake
+import net.tassia.pancake.http.RouteRegistrar
 import net.tassia.pancake.http.event.HttpRegisterRoutesEvent
 import net.tassia.pancake.http.feature.Logging
 import net.tassia.pancake.http.feature.ResponseHeaders
 import net.tassia.pancake.http.route.v1.RegisterRoutesV1
+import net.tassia.pancake.plugin.Plugin
+import net.tassia.pancake.plugin.PluginInfo
 
 /**
  * The base class for the HTTP sub-system of Pancake.
@@ -18,14 +21,20 @@ import net.tassia.pancake.http.route.v1.RegisterRoutesV1
  * @since Pancake 1.0
  * @author Tassilo
  */
-class PancakeHttp(private val pancake: Pancake) {
+class PancakeHttp(override val pancake: Pancake) : Plugin(pancake) {
+
+	override val info: PluginInfo = Info
+
+	override val events = setOf(HttpRegisterRoutesEvent::class)
 
 	/**
 	 * The underlying HTTP engine.
 	 */
-	private val engine: NettyApplicationEngine
+	private var engine: NettyApplicationEngine? = null
 
-	init {
+
+
+	override fun onEnable() {
 		pancake.events.registerEvent(HttpRegisterRoutesEvent::class)
 
 		pancake.logger.info("HTTP | Initializing Netty Server Engine...")
@@ -56,8 +65,12 @@ class PancakeHttp(private val pancake: Pancake) {
 		}
 
 		pancake.logger.info("HTTP | Starting Server...")
-		engine.start(wait = false)
+		engine?.start(wait = false)
 	}
+
+
+
+	override fun onDisable() = stop()
 
 	/**
 	 * Shuts down the HTTP server.
@@ -66,7 +79,30 @@ class PancakeHttp(private val pancake: Pancake) {
 	 * @param timeoutMillis the maximum amount of time to wait until server stops gracefully
 	 */
 	fun stop(gracePeriodMillis: Long = 5000, timeoutMillis: Long = 5000) {
-		engine.stop(gracePeriodMillis, timeoutMillis)
+		engine?.stop(gracePeriodMillis, timeoutMillis)
+	}
+
+
+
+	companion object {
+
+		/**
+		 * The version information for the HTTP plugin.
+		 */
+		val Version = Pancake.VERSION
+
+		/**
+		 * The plugin information for the CLI plugin.
+		 */
+		val Info = PluginInfo(
+			id = "net.tassia:HTTP",
+			name = "HTTP",
+			description = "The HTTP server implementation. Adds the Rest API functionality.",
+			authors = setOf("Tassilo"),
+			version = Version,
+			constructor = ::PancakeHttp
+		)
+
 	}
 
 }
