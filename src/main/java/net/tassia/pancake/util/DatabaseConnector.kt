6ae999bd87b1da0abf84a9cd50a9bf234518a9b0
Tassilo <tassia710@gmail.com>
2021-03-20 19:07:25 +0100

    Update DatabaseConnector.kt

--- src/main/java/net/tassia/pancake/util/DatabaseConnector.kt
+++ src/main/java/net/tassia/pancake/util/DatabaseConnector.kt
@@ -2,7 +2,9 @@ package net.tassia.pancake.util
 
 import net.tassia.pancake.io.DatabaseDriver
 import net.tassia.pancake.io.PancakeConfig
+import net.tassia.pancake.logging.Logger
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.name
 
 /**
  * Utility class used to instantiate database connections.
@@ -19,10 +21,21 @@ object DatabaseConnector {
 	 * @return the database connection
 	 */
 	fun connect(config: PancakeConfig): Database {
-		return when (config.databaseDriver) {
+		Logger.info("Initializing database...")
+
+		// Connect
+		val db = when (config.databaseDriver) {
 			DatabaseDriver.SQLITE -> connectToSQLite()
 			DatabaseDriver.MYSQL -> connectToMySQL(config)
 		}
+
+		// Debug information
+		Logger.debug("- Name: ${db.name}")
+		Logger.debug("- Dialect: ${db.dialect.name}")
+		Logger.debug("- Vendor: ${db.vendor}")
+		Logger.debug("- Version: ${db.version}")
+
+		return db
 	}
 
 
@@ -34,6 +47,7 @@ object DatabaseConnector {
 	 * Connects to an SQLite database.
 	 */
 	private fun connectToSQLite(): Database {
+		Logger.debug("- Using SQLite database...")
 		return Database.connect(SQLITE_URL, SQLITE_DRIVER)
 	}
 
@@ -46,6 +60,7 @@ object DatabaseConnector {
 	 * Connects to a MySQL database.
 	 */
 	private fun connectToMySQL(config: PancakeConfig): Database {
+		Logger.debug("- Using MySQL database...")
 		val url = MYSQL_URL.format(config.mysqlHostname, config.mysqlPort, config.mysqlDatabase)
 		return Database.connect(url, MYSQL_DRIVER, config.mysqlUsername, config.mysqlPassword)
 	}
