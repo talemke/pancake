df2a9cfaec53fad9c7b165364d2449ab898849b0
Tassilo <tassia710@gmail.com>
2021-03-20 18:43:47 +0100

    Create LogFormatter.kt

---
+++ src/main/java/net/tassia/pancake/logging/publisher/LogFormatter.kt
@@ -0,0 +1,21 @@
+package net.tassia.pancake.logging.publisher
+
+import net.tassia.pancake.logging.LogRecord
+
+/**
+ * Formats a given [LogRecord] into a string.
+ *
+ * @since Pancake 1.0
+ * @author Tassilo
+ */
+fun interface LogFormatter {
+
+	/**
+	 * Formats the given log record to a string.
+	 *
+	 * @param record the record
+	 * @return the generated string
+	 */
+	fun format(record: LogRecord): String
+
+}
