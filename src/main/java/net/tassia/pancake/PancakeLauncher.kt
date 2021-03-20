1ae7bfeda1b5a58ae63baf0c190929c192e310a7
Tassilo <tassia710@gmail.com>
2021-03-20 19:51:27 +0100

    Update PancakeLauncher.kt

--- src/main/java/net/tassia/pancake/PancakeLauncher.kt
+++ src/main/java/net/tassia/pancake/PancakeLauncher.kt
@@ -6,9 +6,12 @@ import net.tassia.pancake.util.DatabaseConnector
 import net.tassia.pancake.config.PancakeConfig
 import net.tassia.pancake.logging.Logger
 import net.tassia.pancake.logging.formatter.DefaultFormatter
+import net.tassia.pancake.logging.publisher.FilePublisher
 import net.tassia.pancake.logging.publisher.PrintStreamPublisher
 import net.tassia.pancake.util.PancakeIO
 import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * This object is used to launch a new [Pancake] instance.
@@ -39,9 +42,15 @@ object PancakeLauncher {
 			return@let config
 		}
 
-		// Prepare logger
+		// Add console output publisher to logger
 		Logger.publishers.add(PrintStreamPublisher(
 			stream = System.out,
+			formatter = DefaultFormatter,
+		))
+
+		// Add file publisher to logger
+		Logger.publishers.add(FilePublisher(
+			file = getLogFile(),
 			formatter = DefaultFormatter,
 		))
 
@@ -50,6 +59,19 @@ object PancakeLauncher {
 
 		// Launch
 		return Pancake(cfg)
+	}
+
+
+
+	/**
+	 * Generates a new log file for the current time.
+	 *
+	 * @return the generated log file
+	 */
+	private fun getLogFile(): File {
+		val sdf = SimpleDateFormat("yyyy-MM-dd-'at'-HH-mm")
+		val date = sdf.format(Date())
+		return File("logs", "$date.txt")
 	}
 
 }
