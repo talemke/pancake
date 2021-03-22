67f54863273e3192612c4eeb8f84b518c5af0aa7
Tassilo <tassia710@gmail.com>
2021-03-22 16:41:20 +0100

    Create MailFromCommand.kt

---
+++ src/main/java/net/tassia/smtp/command/MailFromCommand.kt
@@ -0,0 +1,35 @@
+package net.tassia.smtp.command
+
+import net.tassia.pancake.InternetAddress
+import net.tassia.smtp.SMTP
+import net.tassia.smtp.SmtpCommand
+import net.tassia.smtp.SmtpConnection
+
+/**
+ * The MAIL FROM command initiates a mail transfer. As an argument, MAIL FROM includes a sender mailbox (reverse-path).
+ * For some types of reporting messages like non-delivery notifications, the reverse-path may be void.
+ * Optional parameters may also be specified.
+ *
+ * @since PancakeSMTP 1.0
+ * @author Tassilo
+ *
+ * @see <a href="https://blog.mailtrap.io/smtp-commands-and-responses/#MAIL_FROM">MAIL_FROM Command</a>
+ */
+class MailFromCommand(private val sender: InternetAddress) : SmtpCommand() {
+
+	override val responses = setOf(250)
+
+	override fun write(connection: SmtpConnection) {
+		connection.writer.write("$COMMAND:\"$sender\"${SMTP.CRLF}")
+	}
+
+
+
+	companion object {
+
+		@Suppress("SpellCheckingInspection")
+		const val COMMAND = "MAIL FROM"
+
+	}
+
+}
