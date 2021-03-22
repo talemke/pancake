39b1ca6d96ce898ab0d3e27945c1d09ad7354faa
Tassilo <tassia710@gmail.com>
2021-03-22 15:22:24 +0100

    Create SMTP.kt

---
+++ src/main/java/net/tassia/smtp/SMTP.kt
@@ -0,0 +1,19 @@
+package net.tassia.smtp
+
+/**
+ * The base class for the SMTP implementation.
+ * Contains useful constants.
+ *
+ * @since PancakeSMTP 1.0
+ * @author Tassilo
+ */
+object SMTP {
+
+	/**
+	 * A string consisting of the carriage return (CR) and line feed (LN) character.
+	 *
+	 * `<CR><LF>`
+	 */
+	const val CRLF = "\r\n"
+
+}
