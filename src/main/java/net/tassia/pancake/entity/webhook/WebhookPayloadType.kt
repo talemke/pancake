bb006a658215d1928d3be1412fb445b529738228
Tassilo <tassia710@gmail.com>
2021-03-24 14:13:17 +0100

    Create WebhookPayloadType.kt

---
+++ src/main/java/net/tassia/pancake/entity/webhook/WebhookPayloadType.kt
@@ -0,0 +1,19 @@
+package net.tassia.pancake.entity.webhook
+
+/**
+ * Defines the ContentType of a [Webhook].
+ *
+ * @since Pancake 1.0
+ * @author Tassilo
+ */
+enum class WebhookPayloadType {
+
+	/**
+	 * The default JSON content type.
+	 *
+	 * Mime-Type:
+	 * `application/json`
+	 */
+	JSON;
+
+}
