1fd4fba16387b0faf2463e3459667f0534ac1051
Tassilo <tassia710@gmail.com>
2021-03-24 14:47:01 +0100

    Create WebhookEvent.kt

---
+++ src/main/java/net/tassia/pancake/entity/webhook/WebhookEvent.kt
@@ -0,0 +1,26 @@
+package net.tassia.pancake.entity.webhook
+
+/**
+ * Utility object containing constants of webhook events.
+ *
+ * @since Pancake 1.0
+ * @author Tassilo
+ */
+object WebhookEvent {
+
+	/**
+	 * A new mail has been received.
+	 */
+	const val MAIL_RECEIVED = 1L
+
+	/**
+	 * A mail has been moved from one folder to another.
+	 */
+	const val MAIL_MOVED = 2L
+
+	/**
+	 * A mail has been sent.
+	 */
+	const val MAIL_SENT = 4L
+
+}
