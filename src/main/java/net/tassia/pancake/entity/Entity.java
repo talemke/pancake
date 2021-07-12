934922b0823a4370950b4a0e095af0b433654291
Tassilo <tassia710@gmail.com>
2021-07-13 01:19:19 +0200

    Convert to Java

--- src/main/java/net/tassia/pancake/entity/Entity.java
+++ src/main/java/net/tassia/pancake/entity/Entity.java
@@ -1,31 +1,31 @@
-package net.tassia.pancake.entity
+package net.tassia.pancake.entity;
 
 /**
- * The base for all database entities.
+ * The base interface for all database entities.
  *
  * @since Pancake 1.0
  * @author Tassilo
  */
-interface Entity {
+public interface Entity {
 
 	/**
 	 * Reloads this entity. This discards all currently pending changes to this entity
 	 * and resets it to the state as stored in the database.
 	 */
-	fun reload()
+	void reload();
 
 	/**
 	 * Updates this entity. This means that all currently pending changes will be committed
 	 * to the database and this instance is synchronized with the database again.
 	 */
-	fun update()
+	void update();
 
 	/**
 	 * Deletes this entity. This makes sure this entity is erased from the database.
 	 * This method does nothing, if the entity has already been deleted.
 	 *
-	 * **Note:** This function does **not** delete references to this entity.
+	 * <b>Note:</b> This function does <b>not</b> delete references to this entity.
 	 */
-	fun delete()
+	void delete();
 
 }
