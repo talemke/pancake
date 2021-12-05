10a27bb65f49b51cee4aad024f6f19a78334cdc3
Tassilo Lemke <tassilo.lemke@gmail.com>
2021-12-05 14:26:33 +0100

    Add wait until shutdown

--- application/src/main/java/net/tassia/pancake/StandardPancake.kt
+++ application/src/main/java/net/tassia/pancake/StandardPancake.kt
@@ -4,8 +4,10 @@ import net.tassia.pancake.database.StandardDatabase
 import net.tassia.pancake.event.EventManager
 import net.tassia.pancake.plugin.StandardPluginManager
 import net.tassia.pancake.scheduler.StandardScheduler
+import java.util.concurrent.locks.ReentrantLock
 import java.util.logging.Logger
 import javax.sql.DataSource
+import kotlin.concurrent.withLock
 
 class StandardPancake(override val logger: Logger, source: DataSource) : Pancake() {
 
@@ -33,6 +35,18 @@ class StandardPancake(override val logger: Logger, source: DataSource) : Pancake
 
 		// Done!
 		logger.info("Done! Running Pancake v$VERSION")
+		waitForShutdown()
+	}
+
+
+
+	private val shutdownLock = ReentrantLock()
+	private val shutdownCondition = shutdownLock.newCondition()
+
+	private fun waitForShutdown() {
+		shutdownLock.withLock {
+			shutdownCondition.await()
+		}
 	}
 
 }
